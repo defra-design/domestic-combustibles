6c98139adc9eebdff36a2f28d10b9ca9ede6eb1d
rachel-robson-defra <rachel.robson@defra.gov.uk>
2026-03-09 13:53:57 +0000

    first push of list view for appliances

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,4 +2,77 @@ module.exports = {
 
   // Insert values here
 
+  'applianceApplicationInProgress': [
+    //App 1084 Twin Heat M20i, M40i, M60i, M80i
+    {
+      'id': '1084-1',
+      'applicationNo': '1084',
+      'modelName': 'Twin Heat M20i',
+      'reviewer' : 'Sean Keller',
+      'modelNumber': 'M20i',
+      'applianceType': 'Boiler',
+      'isVariant': 'Yes',
+      'variantDetails': 'Another Twin Heat boiler',
+      'thermalOutput': '5'
+    },
+    {
+      'id': '1084-2',
+      'applicationNo': '1084',
+      'modelName': 'Twin Heat M40i',
+      'reviewer' : 'Sean Keller',
+      'modelNumber': 'M40i',
+      'applianceType': 'Boiler',
+      'isVariant': 'Yes',
+      'variantDetails': 'Another Twin Heat boiler',
+      'thermalOutput': '5'
+    },
+    {
+      'id': '1084-3',
+      'applicationNo': '1084',
+      'modelName': 'Twin Heat M60i',
+      'reviewer' : 'Sean Keller',
+      'modelNumber': 'M80i',
+      'applianceType': 'Boiler',
+      'isVariant': 'Yes',
+      'variantDetails': 'Another Twin Heat boiler',
+      'thermalOutput': '3'
+    },
+    {
+      'id': '1084-4',
+      'applicationNo': '1084',
+      'modelName': 'Twin Heat M80i',
+      'reviewer' : 'Sean Keller',
+      'modelNumber': 'M80i',
+      'applianceType': 'Boiler',
+      'isVariant': 'Yes',
+      'variantDetails': 'Another Twin Heat boiler',
+      'thermalOutput': '5'
+    }
+  ],
+  'blah': [
+    {
+      'id': '1083-1',
+      'applicationNo': '1083',
+      'modelName': 'Twin Heat CS200i',
+      'link' : '1083/review-application.html',
+      'modelNumber': 'CS200i',
+      'applianceType': 'Boiler',
+      'isVariant': 'No',
+      'thermalOutput': '1'
+    }
+  ],
+
+  "applianceApplicationNew": [
+    {
+      "id" : 1083-1,
+      "applicationNo": "1083",
+      "modelName": "Twin Heat CS200i"
+    },
+    {
+      "id" : 1093-1,
+      "applicationNo": "1093",
+      "modelName": "Somik"
+    }
+  ]
+
 }
