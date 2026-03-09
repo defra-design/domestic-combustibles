bf581e075cdc347ebfd885ed4a7138ed232811c8
rachel-robson-defra <rachel.robson@defra.gov.uk>
2026-03-09 23:07:57 +0000

    completed appliance listing screens for review

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,64 +1,115 @@
 module.exports = {
 
-  // Insert values here
+  // PUBLIC LIST SERVICE
 
-  'applianceApplicationInProgress': [
-    //App 1084 Twin Heat M20i, M40i, M60i, M80i
+  //Appliances
+  "appliancePublicList": [
     {
-      'id': '1084-1',
-      'applicationNo': '1084',
-      'modelName': 'Twin Heat M20i',
-      'reviewer' : 'Sean Keller',
-      'modelNumber': 'M20i',
-      'applianceType': 'Boiler',
-      'isVariant': 'Yes',
-      'variantDetails': 'Another Twin Heat boiler',
-      'thermalOutput': '5'
+      "applianceName" : "100SE Smoke Exempt Woodburning Stove",
+      "detailLink" : "appliance-details-100SE",
+      "manufacturer" : "ESSE Engineering Limited",
+      "fuels" : "Wood logs",
+      "type" : "Stove",
+      "certified" : "England, Scotland, Wales, Northern Ireland"
     },
     {
-      'id': '1084-2',
-      'applicationNo': '1084',
-      'modelName': 'Twin Heat M40i',
-      'reviewer' : 'Sean Keller',
-      'modelNumber': 'M40i',
-      'applianceType': 'Boiler',
-      'isVariant': 'Yes',
-      'variantDetails': 'Another Twin Heat boiler',
-      'thermalOutput': '5'
+      "applianceName" : "18i and 60i stoves",
+      "detailLink" : "#",
+      "manufacturer" : "Jetmaster Fires Limited",
+      "fuels" : "Wood logs",
+      "type" : "Stove",
+      "certified" : "England, Scotland, Wales, Northern Ireland"
     },
     {
-      'id': '1084-3',
-      'applicationNo': '1084',
-      'modelName': 'Twin Heat M60i',
-      'reviewer' : 'Sean Keller',
-      'modelNumber': 'M80i',
-      'applianceType': 'Boiler',
-      'isVariant': 'Yes',
-      'variantDetails': 'Another Twin Heat boiler',
-      'thermalOutput': '3'
+      "applianceName" : "413STV013-DF/STV13-5KW/137-0020 & 413STV060-DF/STV14-5KW/137-0021",
+      "detailLink" : "#",
+      "manufacturer" : "Mercury Products Ltd",
+      "fuels" : "Wood logs",
+      "type" : "Stove",
+      "certified" : "England, Scotland, Wales, Northern Ireland"
     },
     {
-      'id': '1084-4',
-      'applicationNo': '1084',
-      'modelName': 'Twin Heat M80i',
-      'reviewer' : 'Sean Keller',
-      'modelNumber': 'M80i',
-      'applianceType': 'Boiler',
-      'isVariant': 'Yes',
-      'variantDetails': 'Another Twin Heat boiler',
-      'thermalOutput': '5'
+      "applianceName" : "413STV013S-DF/STV12-4.5KW/137-0019",
+      "detailLink" : "#",
+      "manufacturer" : "Mercury Products Ltd",
+      "fuels" : "Wood logs",
+      "type" : "Stove",
+      "certified" : "England, Scotland, Wales, Northern Ireland"
+    },
+    {
+      "applianceName" : "413STVDL7-DF/STV15-5KW/137-0022 & 413STVT01-DF/STV16-5KW/137-0023",
+      "detailLink" : "#",
+      "manufacturer" : "Mercury Products Ltd",
+      "fuels" : "Wood logs",
+      "type" : "Stove",
+      "certified" : "England, Scotland, Wales, Northern Ireland"
+    },
+    {
+      "applianceName" : "433STV069M-DF/STV07-5KW/137-0011",
+      "detailLink" : "#",
+      "manufacturer" : "Mercury Products Ltd",
+      "fuels" : "Wood logs",
+      "type" : "Stove",
+      "certified" : "England, Scotland, Wales, Northern Ireland"
+    },
+    {
+      "applianceName" : "433STV106M/STV02-8KW/137-0006",
+      "detailLink" : "#",
+      "manufacturer" : "Mercury Products Ltd",
+      "fuels" : "Wood logs",
+      "type" : "Stove",
+      "certified" : "England, Scotland, Wales, Northern Ireland"
+    },
+    {
+      "applianceName" : "433STV106S/STV02-5KW/137-0004",
+      "detailLink" : "#",
+      "manufacturer" : "Mercury Products Ltd",
+      "fuels" : "Wood logs",
+      "type" : "Stove",
+      "certified" : "England, Scotland, Wales, Northern Ireland"
+    },
+    {
+      "applianceName" : "433STV106XS/STV02XS-5KW/137-0005",
+      "detailLink" : "#",
+      "manufacturer" : "Mercury Products Ltd",
+      "fuels" : "Wood logs",
+      "type" : "Stove",
+      "certified" : "England, Scotland, Wales, Northern Ireland"
+    },
+    {
+      "applianceName" : "433STV107/STV03-5KW/137-0007",
+      "detailLink" : "#",
+      "manufacturer" : "Mercury Products Ltd",
+      "fuels" : "Wood logs",
+      "type" : "Stove",
+      "certified" : "England, Scotland, Wales, Northern Ireland"
     }
   ],
-  'blah': [
+
+  // ADMIN SERVICE
+
+  "applianceApplications": [
     {
-      'id': '1083-1',
-      'applicationNo': '1083',
-      'modelName': 'Twin Heat CS200i',
-      'link' : '1083/review-application.html',
-      'modelNumber': 'CS200i',
-      'applianceType': 'Boiler',
-      'isVariant': 'No',
-      'thermalOutput': '1'
+      "id" : 1084-1,
+      "applicationNo": "1084",
+      "modelName": "Twin Heat M20i",
+      "reviewer" : "Sean Keller",
+      "modelNumber": "M20i",
+      "applianceType": "Boiler",
+      "variant": "Yes",
+      "variantDetails": "Another Twin Heat boiler",
+      "thermalOutput": "5"
+    },
+    {
+      "id" : 1084-2,
+      "applicationNo": "1084",
+      "modelName": "Twin Heat M40i",
+      "reviewer" : "Sean Keller",
+      "modelNumber": "M40i",
+      "applianceType": "Boiler",
+      "variant": "Yes",
+      "variantDetails": "Another Twin Heat boiler",
+      "thermalOutput": "3"
     }
   ],
 
