7104909e86276374964a364acb953115c0bfe1c6
rachel-robson-defra <rachel.robson@defra.gov.uk>
2026-05-22 18:55:10 +0100

    building fuel records

--- app/filters.js
+++ app/filters.js
@@ -2,8 +2,3 @@
 // For guidance on how to create filters see:
 // https://prototype-kit.service.gov.uk/docs/filters
 //
-
-const govukPrototypeKit = require('govuk-prototype-kit')
-const addFilter = govukPrototypeKit.views.addFilter
-
-// Add your filters here
